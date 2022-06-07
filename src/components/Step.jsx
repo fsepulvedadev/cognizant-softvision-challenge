cec4026dc24527091893e2a451cccd205a3de5e6
Francisco <panchi_nrc@hotmail.com>
2022-06-06 23:43:36 -0300

    finalizando el layout y parte de la logica

--- src/components/Step.jsx
+++ src/components/Step.jsx
@@ -1,23 +1,18 @@
 import React from "react";
 
-import Candidate from "./Candidate";
-
-const Step = (props) => {
+const Step = ({titulo, first, children}) => {
   return (
     <div className=" rounded overflow-hidden shadow-lg flex bg-white justify-start items-center flex-col h-auto w-full md:w-1/6 mt-4 md:mt-0">
-      <div className="font-bold text-xl mb-2 w-10/12 pt-4">{props.titulo}</div>
-      <Candidate comments="Medio boludo" name="Carlos Candidato" />
-      <Candidate comments="Medio boludo" name="Carlos Candidato" />
-      <Candidate comments="Medio boludo" name="Carlos Candidato" />
-      <Candidate comments="Medio boludo" name="Carlos Candidato" />
-      <Candidate comments="Medio boludo" name="Carlos Candidato" />
-      <Candidate comments="Medio boludo" name="Carlos Candidato" />
+      <div className="font-bold text-xl mb-2 w-10/12 pt-4">{titulo}</div>
+      {children}
 
-      <div className="flex flex-col justify-end h-full">
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-2 rounded my-4">
-          Agregar Candidato
-        </button>
-      </div>
+      {first && (
+        <div className="flex flex-col justify-end h-full">
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-2 rounded my-4">
+            Agregar Candidato
+          </button>
+        </div>
+      )}
     </div>
   );
 };
--- src/components/StepList.jsx
+++ src/components/StepList.jsx
@@ -1,15 +1,84 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
 import Step from "./Step";
+import Candidate from "./Candidate";
 
 const StepList = () => {
+  const [candidates, setCandidates] = useState([]);
+  const [stepOne, setStepOne] = useState([]);
+  const [stepTwo, setStepTwo] = useState([]);
+  const [stepTree, setStepTree] = useState([]);
+  const [stepFour, setStepFour] = useState([]);
+  const [stepFive, setStepFive] = useState([]);
+
+  const clasificarCandidatos = (candidatos) => {
+    candidatos.forEach((candidato) => {
+      const {step} = candidato;
+
+      switch (step) {
+        case "Entrevista inicial":
+          let newStepOne = [...stepOne, candidato];
+
+          setStepOne(newStepOne);
+          break;
+        case "Entrevista técnica":
+          let newStepTwo = [...stepTwo, candidato];
+
+          setStepTwo(newStepTwo);
+          break;
+        case "Oferta":
+          let newStepTree = [...stepTree, candidato];
+
+          setStepTree(newStepTree);
+          break;
+        case "Asignacion":
+          let newStepFour = [...stepFour, candidato];
+
+          setStepFour(newStepFour);
+          break;
+        case "Rechazo":
+          let newStepFive = [...stepFive, candidato];
+
+          setStepFive(newStepFive);
+      }
+    });
+  };
+
+  useEffect(() => {
+    fetch("src/api/candidates.json")
+      .then((res) => res.json())
+      .then((data) => {
+        clasificarCandidatos(data);
+      });
+  }, []);
+
   return (
     <div className="flex md:flex-row flex-col w-full justify-around md:pt-20">
-      <Step titulo="Entrevista Inicial" />
-      <Step titulo="Entrevista Tecnica" />
-      <Step titulo="Oferta" />
-      <Step titulo="Asignacion" />
-      <Step titulo="Rechazo" />
+      <Step first={true} titulo="Entrevista Inicial">
+        {stepOne.map((candidatos) => (
+          <Candidate key={candidatos.id} comments={candidatos.comments} name={candidatos.name} />
+        ))}
+      </Step>
+      <Step titulo="Entrevista Tecnica">
+        {stepTwo.map((candidatos) => (
+          <Candidate key={candidatos.id} comments={candidatos.comments} name={candidatos.name} />
+        ))}
+      </Step>
+      <Step titulo="Oferta">
+        {stepTree.map((candidatos) => (
+          <Candidate key={candidatos.id} comments={candidatos.comments} name={candidatos.name} />
+        ))}
+      </Step>
+      <Step titulo="Asignacion">
+        {stepFour.map((candidatos) => (
+          <Candidate key={candidatos.id} comments={candidatos.comments} name={candidatos.name} />
+        ))}
+      </Step>
+      <Step titulo="Rechazo">
+        {stepFive.map((candidatos) => (
+          <Candidate key={candidatos.id} comments={candidatos.comments} name={candidatos.name} />
+        ))}
+      </Step>
     </div>
   );
 };
